a9bf399191e9961a3fcede29190eeddc6fc62cb3
abbaas1234 <johndoe@example.com>
2024-01-19 08:50:35 +0330

    Finalizing Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Model, ToDoAction } from "./../Models/model";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ToDoApp';
+  model = new Model();
+
+  getName() {
+    return this.model.user;
+  }
+
+  getToDoItems() {
+    return this.model.items;
+  }
+
+  addToDoItem(value: string) {
+    this.model.items.push(new ToDoAction(value, false));
+  }
+
 }
